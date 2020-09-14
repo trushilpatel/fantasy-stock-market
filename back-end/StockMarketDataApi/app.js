372ba82f7defd5b8f97ab021998ef067e6aaaa92
trushilpatel9898 <trushilpatel9898@gmail.com>
2020-09-14 16:05:22 +0530

    env port support added

--- back-end/StockMarketDataApi/app.js
+++ back-end/StockMarketDataApi/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 const StockMarketRouter = require('./src/StockMarket/StockMarket.router')
 
